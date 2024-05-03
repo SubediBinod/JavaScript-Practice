ab529dc37f99d9820c2624c89f1640a0f6282e86
Binod <bidhaksubedi77@gmail.com>
2024-05-03 14:53:55 +0530

    day 7 fifth commit

---
+++ day7/setTimeOut()/index.js
@@ -0,0 +1,25 @@
+// setTimeout() = function in js allows you to schedule 
+// the execution of a function after an amount of time (milliseconds)
+// Times are approximate (varies based on the workload of the javascript runtime env)
+
+// setTimeout(callback,delay)
+
+
+// Define a function to introduce Harry Potter
+function introduceHarry() {
+    console.log("Harry Potter: I'm Harry Potter, the Boy Who Lived!");
+}
+
+// Define a function to introduce Hermione Granger
+function introduceHermione() {
+    console.log("Hermione Granger: I'm Hermione Granger, the brightest witch of my age!");
+}
+
+// Call setTimeout to introduce Harry after 0 seconds
+setTimeout(introduceHarry, 0);
+
+// Call setTimeout to introduce Hermione after a delay of 2 seconds (2000 milliseconds)
+setTimeout(introduceHermione, 2000);
+
+// This message will be printed immediately
+console.log("Introducing Harry Potter and Hermione Granger...");
