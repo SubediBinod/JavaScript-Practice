1a4994cc913a3ecf148d460c5dfd9a6967ba6585
Binod <bidhaksubedi77@gmail.com>
2024-05-04 12:00:19 +0530

    day 9 first commit

---
+++ day9/dom/index.js
@@ -0,0 +1,16 @@
+// DOM ::Document Object Model 
+
+// Object{} that represents the page you see in the web 
+// browser and provides you with an API to interact with it . 
+// Web Browser constructs the Dom when it loadds an HTML document,
+// and structures all the elements in a tree-like representation. 
+
+
+// JavaScript can access the DOM to dynamically 
+// change the SVGTextContentElement, structure, and style of a web page 
+
+document.title="my web page"
+document.body.style.backgroundColor="hsl(0,0%,15%)";
+
+console.log(document)
+console.dir(document)
